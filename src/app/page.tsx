83aa6283063043c09f0e8b144a7770bb0587c154
Jorge Saa <piojo.sistematico@gmail.com>
2024-07-01 21:41:01 -0400

    added autoscroll and a few style changes to the active message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import transcript from "../data/transcript.json";
 import audio from "../data/audio.wav";
+import { useEffect, useRef, useState } from "react";
 
 type TranscriptType = {
   speaker: "Speaker1" | "Speaker2";
@@ -8,24 +11,71 @@ type TranscriptType = {
 };
 
 export default function Home() {
+  const [progress, setProgress] = useState(0);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const ulRef = useRef<HTMLUListElement>(null);
+
+  const activeIndex: number =
+    transcript.findIndex((elem) => Number(elem.syncTime) > progress) - 1;
+
+  function handleClick(time: string) {
+    audioRef.current!.currentTime = Number(time);
+    audioRef.current?.play();
+  }
+
+  useEffect(() => {
+    if (
+      ulRef.current &&
+      ulRef.current.childNodes[activeIndex] instanceof HTMLElement
+    ) {
+      const childElement = ulRef.current.childNodes[activeIndex] as HTMLElement;
+      childElement.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }, [activeIndex]);
+
   return (
-    <main className="bg-gray-900 p-8">
-      <section className=" flex flex-col gap-8">
-        <ul className="flex flex-col gap-4">
+    <main className="bg-gray-900 p-8 h-screen">
+      <section className="flex flex-col gap-8">
+        <audio
+          ref={audioRef}
+          className="w-full"
+          src={audio}
+          controls
+          onTimeUpdate={(e) => setProgress(e.currentTarget.currentTime)}
+        ></audio>
+        <ul
+          ref={ulRef}
+          className="flex flex-col gap-4 h-[600px] overflow-y-auto"
+        >
           {transcript.map((elem, index) => (
             <li
-              className={` text-white py-2 px-4 rounded-md w-3/4 ${
+              onClick={() => handleClick(elem.syncTime)}
+              className={` text-white p-4 rounded-md w-3/4 border border-1 ${
                 elem.speaker == "Speaker2"
                   ? "self-end bg-gray-600"
                   : "bg-gray-700"
+              } ${
+                index === activeIndex
+                  ? "border-red-400 border-2"
+                  : " border-gray-300"
+              } ${
+                progress < Number(elem.syncTime) ? "opacity-50" : "opacity-100"
               }`}
               key={index}
             >
-              {elem.text}
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-row justify-between">
+                  <span className="text-gray-300">{elem.speaker}</span>
+                  <span className="text-gray-400">{elem.syncTime}</span>
+                </div>
+                <p className="text-lg">{elem.text}</p>
+              </div>
             </li>
           ))}
         </ul>
-        <audio className="w-full" src={audio} controls></audio>
       </section>
     </main>
   );
